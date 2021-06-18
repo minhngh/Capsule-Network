fcdf5798101e394b170cbf2345e0f1c708c35322
minhngh <minh.nghminh@gmail.com>
2021-06-18 15:42:38 +0700

    fix convcap layer

--- Dynamic Routing/layers.py
+++ Dynamic Routing/layers.py
@@ -78,12 +78,126 @@ class DigitCapsule(nn.Module):
         return v
 
 
+# class ConvCapsule(nn.Module):
+#     def __init__(self, input_shape, in_capsules, in_cap_dim, out_capsules, out_cap_dim, kernel_size, stride, padding = None, iterations = 3):
+#         super().__init__()
+#         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#         self.padding = padding or (kernel_size - 1) // 2
+#         self.input_shape = (input_shape[0] + 2 * self.padding, input_shape[1] + 2 * self.padding)
+#         self.in_capsules = in_capsules
+#         self.in_cap_dim = in_cap_dim
+#         self.out_capsules = out_capsules
+#         self.out_cap_dim = out_cap_dim
+#         self.iterations = iterations
+#         self.kernel_size = kernel_size
+#         self.stride = stride
+#         self.W = nn.Parameter(torch.randn(1, 1, 1, kernel_size ** 2, in_capsules, out_capsules, in_cap_dim, out_cap_dim, device = device))
+#         self.device = device
+#         self.kernel_map, self.parent_size = self.__get_kernel_map(self.input_shape, self.kernel_size, self.stride)
+#         self.children_per_parent = self.__group_children_by_parent(self.kernel_map)
+
+#         nn.init.xavier_normal_(self.W)
+#     def __init_c(self, kernel_map, parent_shape, in_capsules, out_capsules):
+#         num_parents_per_child = kernel_map.sum(axis = 0, keepdims = True)
+#         initial_c = kernel_map / (num_parents_per_child * out_capsules + 1e-6)
+#         initial_c = initial_c[kernel_map == 1].reshape(*parent_shape, -1)[..., None, None]
+#         initial_c = np.tile(initial_c, [1, 1, 1, in_capsules, out_capsules])[None]
+#         return initial_c
+#     def __update_c(self, b):
+#         #N, ps, ps, ks * ks, in_caps, out_caps
+#         bs = b.size(0)
+#         ph = b.size(1)
+#         pw = b.size(2)
+#         ks2 = b.size(3)
+#         in_caps = b.size(-2)
+#         b = b.permute(0, 3, 4, 1, 2, 5)
+#         b = b.reshape(bs, ks2, in_caps, -1)
+#         c = F.softmax(b, dim = -1)
+#         c = c.reshape(bs, ks2, in_caps, ph, pw, -1)
+#         c = c.permute(0, 3, 4, 1, 2, 5)
+#         return c
+#     def __get_kernel_map(self, child_shape, ks, stride):
+#         parent_h = (child_shape[0] - ks) // stride + 1
+#         parent_w = (child_shape[1] - ks) // stride + 1
+#         kernel_map = np.zeros((parent_h * parent_w, np.prod(child_shape)))
+#         for r in range(parent_h):
+#             for c in range(parent_w):
+#                 p_idx = r * parent_w + c
+#                 for i in range(ks):
+#                     c_idx = r * stride * child_shape[1] + c * stride + i * child_shape[0]
+#                     kernel_map[p_idx, c_idx : c_idx + ks] = 1
+#         return kernel_map, (parent_h, parent_w)
+#     def __group_children_by_parent(self, map):
+#         return np.where(map)[1].reshape(map.shape[0], -1)
+#     def forward(self, x):
+#         x = F.pad(x, (0, 0, 0, 0, self.padding, self.padding, self.padding, self.padding))
+#         kernel_map, parent_size = self.kernel_map, self.parent_size
+#         children_per_parent = self.children_per_parent
+
+#         x_unroll = x.view(x.shape[0], -1, self.in_capsules, self.in_cap_dim)
+#         tile = x_unroll[:, children_per_parent]
+#         tile = tile.view(x.shape[0], *parent_size, self.kernel_size ** 2, self.in_capsules, self.in_cap_dim).unsqueeze(-2).unsqueeze(-2)
+#         u_hat = torch.matmul(tile, self.W).squeeze(-2)
+#         u_hat_detach = u_hat.detach()
+#         b = torch.zeros(x.shape[0], *parent_size, self.kernel_size ** 2, self.in_capsules, self.out_capsules)
+#         for i in range(self.iterations - 1):
+#             if i == 0:
+#                 c = torch.Tensor(self.__init_c(kernel_map, parent_size, self.in_capsules, self.out_capsules), device = self.device)
+#             else:
+#                 c = self.__update_c(b)
+#             # N x ps x ps x o x out_dim
+#             s = torch.sum(c.unsqueeze(-1) * u_hat_detach, dim = [3, 4])
+#             v = squash(s)
+#             b += torch.sum(u_hat_detach * v.unsqueeze(-3).unsqueeze(-3), dim = -1)
+
+#         c = self.__update_c(b)
+#         s = torch.sum(c.unsqueeze(-1) * u_hat, dim = [3, 4])
+#         v = squash(s)
+#         return v
+
+# class ConvTranposeCapsule(nn.Module):
+#     def __init__(self, input_shape, in_capsules, in_cap_dim, out_capsules, out_cap_dim, kernel_size, stride, padding = 0, iterations = 3):
+#         super().__init__()
+#         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#         self.padding = padding
+#         self.input_shape = input_shape
+#         self.in_capsules = in_capsules
+#         self.in_cap_dim = in_cap_dim
+#         self.out_capsules = out_capsules
+#         self.out_cap_dim = out_cap_dim
+#         self.iterations = iterations
+#         self.kernel_size = kernel_size
+#         self.stride = stride
+#         self.W = nn.Parameter(torch.randn(in_cap_dim, out_capsules * out_cap_dim, kernel_size, kernel_size).to(device))
+#         self.device = device
+#         nn.init.xavier_normal_(self.W)
+
+#     def forward(self, x):
+#       in_shape = x.shape
+#       x = x.permute(0, 3, 4, 1, 2)
+#       x = x.reshape(-1, self.in_cap_dim, *self.input_shape)
+#       x = F.conv_transpose2d(x, self.W, stride = self.stride, padding = self.padding)
+#       x = x.view(in_shape[0], in_shape[-2], self.out_capsules, self.out_cap_dim, x.shape[2], x.shape[3])
+#       u_hat = x.permute(0, 1, 4, 5, 2, 3)
+      
+#       b = torch.zeros(u_hat.shape[:-1]).to(self.device)
+#       for _ in range(self.iterations - 1):
+#         c = F.softmax(b, dim = -2)
+#         s = torch.sum(u_hat * c.unsqueeze(-1), dim = 1)
+#         v = squash(s)
+#         b = b + torch.sum(u_hat * v.unsqueeze(1), dim = -1)
+#       c = F.softmax(b, dim = -2)
+#       s = torch.sum(u_hat * c.unsqueeze(-1), dim = 1)
+#       v = squash(s)
+#       return v
+
+
 class ConvCapsule(nn.Module):
-    def __init__(self, input_shape, in_capsules, in_cap_dim, out_capsules, out_cap_dim, kernel_size, stride, padding = None, iterations = 3):
+    def __init__(self, input_shape, in_capsules, in_cap_dim, out_capsules, out_cap_dim, kernel_size, stride, padding = 0, iterations = 3):
         super().__init__()
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.padding = padding or (kernel_size - 1) // 2
-        self.input_shape = (input_shape[0] + 2 * self.padding, input_shape[1] + 2 * self.padding)
+        self.padding = padding
+        self.input_shape = input_shape
         self.in_capsules = in_capsules
         self.in_cap_dim = in_cap_dim
         self.out_capsules = out_capsules
@@ -91,66 +205,25 @@ class ConvCapsule(nn.Module):
         self.iterations = iterations
         self.kernel_size = kernel_size
         self.stride = stride
-        self.W = nn.Parameter(torch.randn(1, 1, 1, kernel_size ** 2, in_capsules, out_capsules, in_cap_dim, out_cap_dim, device = device))
+        self.W = nn.Parameter(torch.randn(out_capsules * out_cap_dim, in_cap_dim, kernel_size, kernel_size, device = device))
         self.device = device
-        self.kernel_map, self.parent_size = self.__get_kernel_map(self.input_shape, self.kernel_size, self.stride)
-        self.children_per_parent = self.__group_children_by_parent(self.kernel_map)
-
         nn.init.xavier_normal_(self.W)
-    def __init_c(self, kernel_map, parent_shape, in_capsules, out_capsules):
-        num_parents_per_child = kernel_map.sum(axis = 0, keepdims = True)
-        initial_c = kernel_map / (num_parents_per_child * out_capsules + 1e-6)
-        initial_c = initial_c[kernel_map == 1].reshape(*parent_shape, -1)[..., None, None]
-        initial_c = np.tile(initial_c, [1, 1, 1, in_capsules, out_capsules])[None]
-        return initial_c
-    def __update_c(self, b):
-        #N, ps, ps, ks * ks, in_caps, out_caps
-        bs = b.size(0)
-        ph = b.size(1)
-        pw = b.size(2)
-        ks2 = b.size(3)
-        in_caps = b.size(-2)
-        b = b.permute(0, 3, 4, 1, 2, 5)
-        b = b.reshape(bs, ks2, in_caps, -1)
-        c = F.softmax(b, dim = -1)
-        c = c.reshape(bs, ks2, in_caps, ph, pw, -1)
-        c = c.permute(0, 3, 4, 1, 2, 5)
-        return c
-    def __get_kernel_map(self, child_shape, ks, stride):
-        parent_h = (child_shape[0] - ks) // stride + 1
-        parent_w = (child_shape[1] - ks) // stride + 1
-        kernel_map = np.zeros((parent_h * parent_w, np.prod(child_shape)))
-        for r in range(parent_h):
-            for c in range(parent_w):
-                p_idx = r * parent_w + c
-                for i in range(ks):
-                    c_idx = r * stride * child_shape[1] + c * stride + i * child_shape[0]
-                    kernel_map[p_idx, c_idx : c_idx + ks] = 1
-        return kernel_map, (parent_h, parent_w)
-    def __group_children_by_parent(self, map):
-        return np.where(map)[1].reshape(map.shape[0], -1)
+
     def forward(self, x):
-        x = F.pad(x, (0, 0, 0, 0, self.padding, self.padding, self.padding, self.padding))
-        kernel_map, parent_size = self.kernel_map, self.parent_size
-        children_per_parent = self.children_per_parent
-
-        x_unroll = x.view(x.shape[0], -1, self.in_capsules, self.in_cap_dim)
-        tile = x_unroll[:, children_per_parent]
-        tile = tile.view(x.shape[0], *parent_size, self.kernel_size ** 2, self.in_capsules, self.in_cap_dim).unsqueeze(-2).unsqueeze(-2)
-        u_hat = torch.matmul(tile, self.W).squeeze(-2)
-        u_hat_detach = u_hat.detach()
-        b = torch.zeros(x.shape[0], *parent_size, self.kernel_size ** 2, self.in_capsules, self.out_capsules)
-        for i in range(self.iterations - 1):
-            if i == 0:
-                c = torch.Tensor(self.__init_c(kernel_map, parent_size, self.in_capsules, self.out_capsules), device = self.device)
-            else:
-                c = self.__update_c(b)
-            # N x ps x ps x o x out_dim
-            s = torch.sum(c.unsqueeze(-1) * u_hat_detach, dim = [3, 4])
-            v = squash(s)
-            b += torch.sum(u_hat_detach * v.unsqueeze(-3).unsqueeze(-3), dim = -1)
-
-        c = self.__update_c(b)
-        s = torch.sum(c.unsqueeze(-1) * u_hat, dim = [3, 4])
+      in_shape = x.shape
+      x = x.permute(0, 3, 4, 1, 2)
+      x = x.reshape(-1, self.in_cap_dim, *self.input_shape)
+      x = F.conv2d(x, self.W, stride = self.stride, padding = self.padding)
+      x = x.view(in_shape[0], in_shape[-2], self.out_capsules, self.out_cap_dim, x.shape[2], x.shape[3])
+      u_hat = x.permute(0, 1, 4, 5, 2, 3)
+      
+      b = torch.zeros(u_hat.shape[:-1]).to(self.device)
+      for _ in range(self.iterations - 1):
+        c = F.softmax(b, dim = -2)
+        s = torch.sum(u_hat * c.unsqueeze(-1), dim = 1)
         v = squash(s)
-        return v
+        b = b + torch.sum(u_hat * v.unsqueeze(1), dim = -1)
+      c = F.softmax(b, dim = -2)
+      s = torch.sum(u_hat * c.unsqueeze(-1), dim = 1)
+      v = squash(s)
+      return v
--- Dynamic Routing/models.py
+++ Dynamic Routing/models.py
@@ -14,40 +14,63 @@ class CapNet(nn.Module):
         x = self.digit_cap(x)
         return x
         
+# class UCapsuleNet(nn.Module):
+#     def __init__(self, image_shape, in_channels):
+#         super().__init__()
+#         self.conv1 = nn.Conv2d(in_channels, 256, kernel_size = 9, padding = 4)
+#         self.relu1 = nn.ReLU(inplace = True)
+#         self.primary_capsule = PrimaryCapsule(256, 16, 8)
+#         # shape1 = get_subsequent_shape(image_shape, 9, 2)
+
+#         self.down_cap1 = ConvCapsule((10, 10), 2, 8, 4, 16, 3, 2, 1)
+#         self.up_cap1 = ConvCapsule((10, 10), 4, 16, 2, 8, 3, 1, 1)
+
+#         self.up_conv = nn.ConvTranspose2d(32, 1, kernel_size = 9, stride = 2, output_padding = 1)
+
+#     def up(self, x, scale_factor):
+#         # N x H x W x C x d
+#         in_dim = x.shape[-1]
+#         in_caps = x.shape[-2]
+#         h = x.shape[1]
+#         w = x.shape[2]
+#         x = x.permute(0, 3, 4, 1, 2)
+#         x = x.reshape(x.shape[0], -1, h, w)
+#         x = F.interpolate(x, scale_factor = scale_factor).reshape(x.shape[0], in_caps, in_dim, h * scale_factor, w * scale_factor)
+#         x = x.permute(0, 3, 4, 1, 2)
+#         return x
+        
+#     def forward(self, x):
+#         x = self.conv1(x)
+#         x = self.relu1(x)
+#         primary_capsule = self.primary_capsule(x)
+#         out_down1 = self.down_cap1(primary_capsule)
+#         out_up1 = self.up_cap1(self.up(out_down1, 2))
+#         out = torch.cat((primary_capsule, out_up1), dim = -1)
+#         out = out.view(out.shape[0], out.shape[1], out.shape[2], -1)
+#         out = out.permute(0, 3, 1, 2)
+#         out = self.up_conv(out)
+
+#         return torch.sigmoid(out)
+
 class UCapsuleNet(nn.Module):
     def __init__(self, image_shape, in_channels):
         super().__init__()
-        self.conv1 = nn.Conv2d(in_channels, 256, kernel_size = 9, padding = 4)
+        self.conv1 = nn.Conv2d(in_channels, 64, kernel_size = 3, padding = 1)
         self.relu1 = nn.ReLU(inplace = True)
-        self.primary_capsule = PrimaryCapsule(256, 16, 8)
-        # shape1 = get_subsequent_shape(image_shape, 9, 2)
+        self.primary_capsule = PrimaryCapsule(64, 2, 2)
 
-        self.down_cap1 = ConvCapsule((10, 10), 2, 8, 4, 16, 3, 2, 1)
-        self.up_cap1 = ConvCapsule((10, 10), 4, 16, 2, 8, 3, 1, 1)
+        self.down_cap1 = ConvCapsule((14, 14), 1, 2, 1, 2, 3, 2, 1)
+        self.up_cap1 = ConvTranposeCapsule((7, 7), 1, 2, 1, 2, 4, 2, 1)
+        self.re_conv1 = nn.ConvTranspose2d(4, 1, kernel_size = 4, stride = 2, padding = 1)
 
-        self.up_conv = nn.ConvTranspose2d(32, 1, kernel_size = 9, stride = 2, output_padding = 1)
-
-    def up(self, x, scale_factor):
-        # N x H x W x C x d
-        in_dim = x.shape[-1]
-        in_caps = x.shape[-2]
-        h = x.shape[1]
-        w = x.shape[2]
-        x = x.permute(0, 3, 4, 1, 2)
-        x = x.reshape(x.shape[0], -1, h, w)
-        x = F.interpolate(x, scale_factor = scale_factor).reshape(x.shape[0], in_caps, in_dim, h * scale_factor, w * scale_factor)
-        x = x.permute(0, 3, 4, 1, 2)
-        return x
-        
     def forward(self, x):
         x = self.conv1(x)
         x = self.relu1(x)
         primary_capsule = self.primary_capsule(x)
         out_down1 = self.down_cap1(primary_capsule)
-        out_up1 = self.up_cap1(self.up(out_down1, 2))
-        out = torch.cat((primary_capsule, out_up1), dim = -1)
-        out = out.view(out.shape[0], out.shape[1], out.shape[2], -1)
+        out_up1 = self.up_cap1(out_down1)
+        out_cat1 = torch.cat((primary_capsule, out_up1), dim = -2)
+        out = out_cat1.reshape(*out_cat1.shape[:3], -1)
         out = out.permute(0, 3, 1, 2)
-        out = self.up_conv(out)
-
+        out = self.re_conv1(out)
         return torch.sigmoid(out)
